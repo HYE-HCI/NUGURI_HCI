1108889d30da0e02a5466be54478912beeddd312
KDohoon <106829307+KDohoon@users.noreply.github.com>
2024-11-13 17:22:26 +0900

    데이터 베이스 자손파일

---
+++ NUGURI/app_NUGURI/management/commands/export_products.py
@@ -0,0 +1,18 @@
+# app_NUGURI/management/commands/export_products.py
+import os
+from django.core.management.base import BaseCommand
+from django.core import serializers
+from app_NUGURI.models import Product
+
+class Command(BaseCommand):
+    help = 'Export all products to a JSON file'
+
+    def handle(self, *args, **kwargs):
+        # 폴더가 없으면 생성
+        os.makedirs('static/data', exist_ok=True)
+        
+        # UTF-8 인코딩으로 파일을 저장
+        products = Product.objects.all()
+        with open('static/data/products.json', 'w', encoding='utf-8') as f:
+            f.write(serializers.serialize('json', products))
+        self.stdout.write(self.style.SUCCESS('Products exported to static/data/products.json'))
